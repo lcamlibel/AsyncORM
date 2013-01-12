68bc206f7c7d91e737c7507c6dffcba4c322715d
lcamlibel <lcamlibel@outsitenetworks.com>
2013-01-12 03:58:36 -0500

    Added test cases

---
+++ AsyncORM.UnitTests/DynamicQueryTest.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using AsyncORM.interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AsyncORM.UnitTests
+{
+    [TestClass]
+    public class DynamicQueryTest
+    {
+        [TestMethod]
+        public async Task ExecuteAsync_Success_NoParameter()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+            IQueryAsync dynamicQuery = new DynamicQuery(connString);
+            IEnumerable<dynamic> result =
+                await dynamicQuery.ExecuteAsync("select top 1 * from [HumanResources].[Department]");
+            Assert.IsTrue(result.Any());
+        }
+
+        [TestMethod]
+        public async Task ExecuteAsync_Success_WithParameter()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+            IQueryAsync dynamicQuery = new DynamicQuery(connString);
+            IEnumerable<dynamic> result =
+                await
+                dynamicQuery.ExecuteAsync(
+                    "select top 1 * from [HumanResources].[Department] where DepartmentId=@DepartmentId",
+                    new {DepartmentId = 8});
+            Assert.IsTrue(result.Any());
+        }
+        [TestMethod]
+        public async Task ExecuteAsync_Verify_WithParameter()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+            IQueryAsync dynamicQuery = new DynamicQuery(connString);
+            IEnumerable<dynamic> result =
+                await
+                dynamicQuery.ExecuteAsync(
+                    "select top 1 * from [HumanResources].[Department] where DepartmentId=@DepartmentId",
+                    new { DepartmentId = 8 });
+            Assert.IsTrue(result.Any(x => x.DepartmentID==8));
+        }
+        [TestMethod]
+        public async Task ExecuteAsync_Projection_NoParameter()
+        {
+            string connString = ConfigurationManager.ConnectionStrings["test"].ConnectionString;
+            IQueryAsync dynamicQuery = new DynamicQuery(connString);
+            IEnumerable<dynamic> result =
+                await
+                dynamicQuery.ExecuteAsync("select * from [HumanResources].[Department]");
+
+            var departments = result.Select(x => new {DepartmentId = x.DepartmentID, DeptName = x.Name});
+            Assert.IsTrue(departments.Any());
+        }
+    }
+}
